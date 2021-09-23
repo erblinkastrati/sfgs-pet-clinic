3d5f68b21d3e5e8d123dad50c61143346687b7b3
Erblin Kastrati <kastratierblin@gmail.com>
2021-09-23 09:32:31 +0200

    Initial commit with Spring stuff

---
+++ src/main/java/com/example/springguru/petclinic/sfgspetclinic/SfgsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springguru.petclinic.sfgspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgsPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springguru/petclinic/sfgspetclinic/SfgsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springguru.petclinic.sfgspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
