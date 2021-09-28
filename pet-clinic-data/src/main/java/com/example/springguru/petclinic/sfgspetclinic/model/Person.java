0674e787954f8bffbe78d3b0ddfb5415f62cc28e
Erblin Kastrati <kastratierblin@gmail.com>
2021-09-28 21:44:37 +0200

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/com/example/springguru/petclinic/sfgspetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/springguru/petclinic/sfgspetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.example.springguru.petclinic.sfgspetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
